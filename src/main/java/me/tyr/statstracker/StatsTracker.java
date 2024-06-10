c3880cb419ed7224d8fbb3867a2d6422c810461c
quentintyr <quentin.tyr@gmail.com>
2024-06-10 23:23:32 +0200

    ADD | test code for server test

--- src/main/java/me/tyr/statstracker/StatsTracker.java
+++ src/main/java/me/tyr/statstracker/StatsTracker.java
@@ -2,16 +2,18 @@ package me.tyr.statstracker;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.sql.SQLOutput;
+
 public final class StatsTracker extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        System.out.println("StatsTracker has been enabled!");
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        System.out.println("StatsTracker has been disabled!");
     }
 }
